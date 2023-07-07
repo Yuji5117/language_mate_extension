971585f370b479421f62d970522e5eb99710b65f
Yuji Nakamura <ny5117@icloud.com>
2023-07-07 16:42:10 +0900

    簡易的なフォームを実装

--- src/app/features/translater/Form.tsx
+++ src/app/features/translater/Form.tsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { translate } from './api';
 
 const Form = () => {
-  return (
-    <form>
-      <div className="mb-4">
-        <label htmlFor="name" className="block text-sm font-medium text-gray-600">
-          Word
-        </label>
-        <input type="text" id="name" name="name" className="mt-1 p-2 w-full border rounded-md" />
-      </div>
+  const [word, setWord] = useState('');
+  const [inputText, setInputText] = useState('');
 
-      <button type="submit" className="w-full p-2 bg-blue-500 text-white rounded-md">
-        Translate
-      </button>
-    </form>
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => setInputText(e.target.value);
+
+  return (
+    <>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const res: string = await translate(inputText);
+          setWord(res);
+        }}
+      >
+        <div className="mb-4">
+          <label htmlFor="name" className="block text-sm font-medium text-gray-600">
+            Word
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="mt-1 p-2 w-full border rounded-md"
+            onChange={(e) => onChangeInput(e)}
+          />
+        </div>
+
+        <button type="submit" className="w-full p-2 bg-blue-500 text-white rounded-md">
+          Translate
+        </button>
+      </form>
+      <div className="pt-6 whitespace-pre-line">{word ? word : 'nothing'}</div>
+    </>
   );
 };
 
--- src/app/features/translater/Translater.tsx
+++ src/app/features/translater/Translater.tsx
@@ -4,7 +4,6 @@ const Translater = () => {
   return (
     <div className="p-6 bg-white rounded shadow-md">
       <Form />
-      <div className="pt-6">ここに結果が出る</div>
     </div>
   );
 };
